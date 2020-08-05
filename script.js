f58fda928ddfaeade0d1000d3e739200f0c36111
Supratik <rikc.2000@gmail.com>
2020-08-05 23:14:35 +0530

    changed delay in script

--- script.js
+++ script.js
@@ -22,19 +22,19 @@ sr.reveal('.animate-right', {
 	origin: 'right',
 	duration: 1000,
 	distance: '25rem',
-	delay: 600
+	delay: 300
 });
 
 sr.reveal('.animate-top', {
 	origin: 'top',
 	duration: 1000,
 	distance: '25rem',
-	delay: 600
+	delay: 300
 });
 
 sr.reveal('.animate-bottom', {
 	origin: 'bottom',
 	duration: 1000,
 	distance: '25rem',
-	delay: 600
+	delay: 300
 });
